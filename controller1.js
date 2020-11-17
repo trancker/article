41f1180bf7c80eb47c4e48b5549630f7bed08a8f
tanzil786 <tanzilidrisi68@gmail.com>
2020-11-17 17:01:23 +0530

    Initial commit

---
+++ controller1.js
@@ -0,0 +1,38 @@
+window.onload = () => {
+    const navMenu = document.querySelector('.nav-menu');
+    const navItems = document.querySelectorAll('.nav-item');
+    const hamburger = document.querySelector('.nav-toggle');
+    
+    const toggle = e => e.classList.toggle('is-active');
+    const toggleNav = ({ target }) => Array.from(navMenu.classList).includes('is-active') ? toggle(navMenu) : null;
+  
+    hamburger.addEventListener('click', () => toggle(navMenu, 'is-active'));
+    Array.from(navItems).forEach(e => e.addEventListener('click', toggleNav));
+  }
+  
+  
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}  
